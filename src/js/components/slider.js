70b47b1fef018542a4b93052e19fe691ae2638dd
Pavel Makarov <pahhhan1@gmail.com>
2024-08-31 17:15:47 +0300

    Homework, added to the partners swiper section and pagination to the insight section

--- src/js/components/slider.js
+++ src/js/components/slider.js
@@ -1,6 +1,6 @@
 import Swiper from 'swiper';
-// import Swiper styles
-import 'swiper/css';
+import { Autoplay, Pagination } from 'swiper/modules';
+Swiper.use([Autoplay, Pagination]);
 
 export const useInsightSlider = () => {
     new Swiper('.insight__slider', {
@@ -8,10 +8,66 @@ export const useInsightSlider = () => {
         spaceBetween: 32,
         loop: true,
         centeredSlides: true,
+
+        pagination: {
+            el: '.insight__pagination',
+            clickable: true,
+        },
+
         breakpoints: {
             993: {
                 centeredSlides: false,
+            },
+        },
+    });
+};
+
+export const usePartnersSlider = () => {
+    let partnersSlider = null;
+
+    function checkWidth() {
+        if (window.innerWidth <= 992) {
+            if (!partnersSlider) {
+                initSwiper();
+            }
+        } else {
+            if (partnersSlider) {
+                destroySwiper();
             }
         }
-    });
+    }
+
+    function initSwiper() {
+        partnersSlider = new Swiper('.partners__slider', {
+            modules: [Autoplay],
+            speed: 1000,
+            loop: true,
+            slidesPerView: 1.5,
+            spaceBetween: 15,
+            centeredSlides: true,
+            autoplay: {
+                delay: 2000,
+                disableOnInteraction: false,
+            },
+            breakpoints: {
+                451: {
+                    slidesPerView: 2,
+                    spaceBetween: 30,
+                },
+                651: {
+                    slidesPerView: 3,
+                },
+            },
+        });
+    }
+
+    function destroySwiper() {
+        if (partnersSlider) {
+            partnersSlider.destroy(true, true);
+            partnersSlider = null;
+        }
+    }
+
+    window.addEventListener('resize', checkWidth);
+    checkWidth();
 };
--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,14 @@
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/autoplay';
 import '../assets/scss/main.scss';
 
 // components
 import { useTheme } from './components/theme.js';
-import {useBurger} from './components/burger.js';
-import {useInsightSlider} from './components/slider.js';
+import { useBurger } from './components/burger.js';
+import { useInsightSlider } from './components/slider.js';
+import { usePartnersSlider } from './components/slider.js';
 useTheme();
 useBurger();
 useInsightSlider();
-
+usePartnersSlider();
