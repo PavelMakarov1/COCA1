449b627dbb336be6c392bd4779b48e3659ff4fdb
Pavel Makarov <pahhhan1@gmail.com>
2024-09-12 17:03:14 +0300

    Created contact: hero and added contact: partners section

---
+++ src/js/contact.js
@@ -0,0 +1,17 @@
+import 'swiper/css';
+import 'swiper/css/autoplay';
+import '../assets/scss/contact.scss';
+import './components/footer.js';
+
+// components
+import { useTheme } from './components/theme.js';
+import { useBurger } from './components/burger.js';
+import { useMap } from './components/contact/contactMap.js';
+import { useTestimonialsSlider } from './components/home/slider.js';
+import { usePartnersSlider } from './components/home/slider.js';
+
+useTheme();
+useBurger();
+useMap();
+useTestimonialsSlider();
+usePartnersSlider();
